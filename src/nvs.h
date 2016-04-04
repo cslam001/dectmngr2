020e4e619f30e43c3db16e666afc6de862c48569
Ronny Nilsson <ronny.nilsson@inteno.se>
2016-04-04 14:09:08 +0200

    - Live patching of NVS with Dect calibration frequency and Equipment Manufacturer Code from nvram.

--- src/nvs.h
+++ src/nvs.h
@@ -12,6 +12,8 @@ void nvs_init(void * event_base, config_t * config);
 int nvs_file_write(uint32_t offset, uint32_t len, uint8_t *data);
 int nvs_file_read(uint32_t *len, uint8_t *data);
 int nvs_rfpi_patch(uint8_t *data);
+int nvs_freq_patch(uint8_t *data);
+int nvs_emc_patch(uint8_t *data);
 
 
 #endif
