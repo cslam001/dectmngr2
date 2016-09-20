c084e7de33394bb786a0b8158556bfd3f743aab0
Ronny Nilsson <ronny.nilsson@inteno.se>
2016-09-20 15:09:13 +0200

    Comments

--- files/dectmngr-wrapper.sh
+++ files/dectmngr-wrapper.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
 # Unload and re-load Dect firmware stack to force a SW reset
+
+# Insert kernel module if internal Dect
 grep -qE "^dect[[:space:]]" /proc/modules && rmmod dect
 [ -s /lib/modules/*/extra/dect.ko ] && insmod /lib/modules/*/extra/dect.ko
 
+
+# Start dectmngr2 and wait for it to exit
 dectmngr2 $*
 res=$?
 
-# Unload dect firmware stack
+
+# Unload internal dect firmware stack
 grep -qE "^dect[[:space:]]" /proc/modules && rmmod dect
+
 
 # Exit with same status as from Dectmngr2
 exit $res
